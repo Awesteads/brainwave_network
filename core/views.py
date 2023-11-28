52b3e2b744958802df1ce5b0a5cb390e77de32a8
GabrielSpdf <gb.spada@hotmail.com>
2023-11-28 07:07:14 -0300

    FINALIZADOv1

--- core/views.py
+++ core/views.py
@@ -7,19 +7,20 @@ from django.db.models import Count
 from post.models import Post
 from .forms import SignupForm, LoginForm
 
-def index(request):
-    items = Item.objects.filter(is_sold=False)[0:6]
-    categories = Category.objects.all()
-
-    return render(request, 'core/index.html', {
-        'categories': categories,
-        'items': items,
-    })
-
 def contact(request):
     return render(request, 'core/contact.html')
 
 def brainwave(request):
+    # Obtenha os posts ordenados pela contagem de curtidas (do maior para o menor)
+    posts_by_likes = Post.objects.annotate(likes_count=Count('likes')).order_by('-likes_count')
+
+    # Obtenha os posts ordenados pela data de criação (do mais recente para o mais antigo)
+    posts_by_date = Post.objects.all().order_by('-created_at')
+
+    # Renderize o template 'brainwave.html' com as duas listas de posts
+    return render(request, 'core/brainwave.html', {'posts_by_likes': posts_by_likes, 'posts_by_date': posts_by_date})
+
+def index(request):
     # Obtenha os posts ordenados pela contagem de curtidas (do maior para o menor)
     posts_by_likes = Post.objects.annotate(likes_count=Count('likes')).order_by('-likes_count')
 
